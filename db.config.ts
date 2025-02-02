fc2f28a7a1affd391cf852698ebfcfec9bdb3ce3
vaneath <vaneathzz@gmail.com>
2025-02-02 13:21:26 +0700

    close #3 setup type orm with postgres for database

---
+++ db.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as path from 'path';
+
+export default registerAs(
+  'database',
+  (): PostgresConnectionOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'srok_sart',
+    entities: [path.join(__dirname, '/**/*.entity{.ts,.js}')],
+    synchronize: process.env.DB_SYNC === 'true' || false,
+  }),
+);
