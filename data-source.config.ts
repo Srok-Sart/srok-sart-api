6ddb0f96ddbd53d0d9d0d88a6ae77779488b8031
Ponloe <sovannsoponloe@gmail.com>
2025-02-24 19:07:34 +0700

    Refactor data source configuration and update fixture files for improved data generation

---
+++ data-source.config.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'srok_sart',
+  entities: [path.join(__dirname, '/**/*.entity{.ts,.js}')],
+  migrations: ['src/migrations/*-migration.ts'],
+  migrationsRun: false,
+  logging: true,
+});
+
+export default AppDataSource;
--- src/processor/userProcessor.ts
+++ src/processor/userProcessor.ts
@@ -1,8 +1,19 @@
-import { User } from 'src/users/entities/user.entity';
+import { User } from '../users/entities/user.entity';
 import { IProcessor } from 'typeorm-fixtures-cli';
+import * as argon2 from 'argon2';
 
 export default class UserProcessor implements IProcessor<User> {
-  postProcess(name: string, object: { [key: string]: any }): void {
-    object.name = `${object.firstName} ${object.lastName}`;
+  async postProcess(name: string, object: { [key: string]: any }): Promise<void> {
+    try {
+      if (object.password) {
+        object.password = await argon2.hash(object.password);
+      }
+
+      if (!object.username) {
+        object.username = `${object.firstName.toLowerCase()}_${object.lastName.toLowerCase()}`;
+      }
+    } catch (error) {
+      throw new Error(`Error processing user: ${error.message}`);
+    }
   }
 }
