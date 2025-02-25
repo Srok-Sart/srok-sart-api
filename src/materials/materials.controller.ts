29aa6cd7b434d4b27849012bd34cb38b21d86a92
Vann Vicheka <150057081+VVicheka@users.noreply.github.com>
2025-02-25 22:46:10 +0700

    close #47 add public to material endpoint

--- src/materials/materials.controller.ts
+++ src/materials/materials.controller.ts
@@ -10,26 +10,31 @@ import {
 import { MaterialsService } from './materials.service';
 import { CreateMaterialDto } from './dto/create-material.dto';
 import { UpdateMaterialDto } from './dto/update-material.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('materials')
 export class MaterialsController {
   constructor(private readonly materialsService: MaterialsService) {}
 
+  @Public()
   @Post()
   create(@Body() createMaterialDto: CreateMaterialDto) {
     return this.materialsService.create(createMaterialDto);
   }
 
+  @Public()
   @Get()
   findAll() {
     return this.materialsService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.materialsService.findOne(+id);
   }
 
+  @Public()
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +43,7 @@ export class MaterialsController {
     return this.materialsService.update(+id, updateMaterialDto);
   }
 
+  @Public()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.materialsService.remove(+id);
